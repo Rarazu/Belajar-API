2f6e3c6cc30d7371bd693838296fa1a9febf22ff
Rarazu <rara_zahra_29rpl@student.smktelkom-mlg.sch.id>
2022-02-03 08:56:25 +0700

    sorting data

---
+++ sorting.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const app = express()
+
+// set to read a request data from JSON format
+app.use(express.json())
+
+app.post("/sorting", (request,response) => {
+    data = request.body.data
+    key = request.body.key
+    type = request.body.type
+    let result
+    for (let i = 0; i < data.length; i++) {
+        if (type == 'ascending') {
+            if (key == 'nis') {
+                 result = data.sort(function (a,b) { return a.nis - b.nis})
+            } else if (key == 'nama') {
+                result = data.sort(function(a, b){
+                    if(a.nama < b.nama) { return -1; }
+                    if(a.nama > b.nama) { return 1; }
+                    return 0;
+                })
+            } else if (key == 'umur') {
+                result = data.sort(function (a,b) { return a.umur - b.umur})
+            }
+        } else if (type == 'descending') {
+            if (key == 'nis') {
+                result = data.sort(function (a,b) { return b.nis - a.nis})
+            } else if (key == 'nama') {
+                result = data.sort(function(a, b){
+                    if(a.nama < b.nama) { return 1}
+                    if(a.nama > b.nama) { return -1}
+                    return 0
+                })
+            } else if (key == 'umur') {
+                result = data.sort(function (a,b) { return b.umur - a.umur})
+            }
+        }
+
+        
+    }
+    return response.json({
+       "result" : result
+    })
+    
+})
+
+app.listen(8001, () => {
+    console.log(`Server run on port 8001`);
+})
